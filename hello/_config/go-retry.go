8c6f56e314314015823d19c73b9b612e567123fa
momo <muzashi777@gmail.com>
2025-12-07 11:48:36 +0800

    config NewGoRetry

--- hello/_config/go-retry.go
+++ hello/_config/go-retry.go
@@ -3,6 +3,7 @@ package config
 import (
 	"example/hello/entity"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/avast/retry-go/v5"
@@ -11,66 +12,59 @@ import (
 type GoRetry struct {
 	Retrier *retry.Retrier
 }
+type DelayContext interface {
+	Delay() time.Duration
+	MaxJitter() time.Duration
+	MaxDelay() time.Duration
+}
 
 func NewGoRetry(retryIfFunc func(error) bool) *GoRetry {
-	fmt.Println("START INIT RETRIER")
-	// retryIf := retry.RetryIf(func(err error) bool {
-	// 	if err.Error() == "special error" {
-	// 		fmt.Println("2")
-	// 		return false
-	// 	}
-	// 	return true
-	// })
+	fmt.Println("---- START INIT RETRIER ----")
+	delay := retry.Delay(1 * time.Second)
+	maxJitter := retry.MaxJitter(100 * time.Millisecond)
+	maxDelay := retry.MaxDelay(1 * time.Second)
+	delayType := retry.DelayType(retry.DelayTypeFunc(delayFunction)) // retry.BackOffDelay() retry.FixedDelay()
 
 	retryIf := retry.RetryIf(retryIfFunc)
 	retryAttempt := retry.Attempts(6)
-	// retryDelay := retry.Delay(1 * time.Second)
 	onretry := retry.OnRetry(func(n uint, err error) {
-		// log.Printf("#%d: %s\n", n, err)
+		log.Printf("Do someting  #%d: %s\n", n+1, err)
 	})
+	attemptsForError := retry.AttemptsForError(3, entity.ERRUnusual)
 
-	delayTypeFunc := retry.DelayTypeFunc(func(n uint, err error, config retry.DelayContext) time.Duration {
-
-		switch err.Error() {
-		case entity.ERRUnusual.Error():
-			fmt.Printf("⚠️ [Error unusual case, retry immeadiatly\n")
-			return 100 * time.Millisecond
-
-		case entity.ERRBusy1.Error():
-			fmt.Printf("⚠️[Error Aware Delay]Database Lock Detected: Long Delay (%v)\n", 3*time.Second)
-			return 3 * time.Second
-
-		case entity.ERRBusy2.Error():
-			baseDelay := (500 * time.Millisecond)
-			delay := baseDelay * time.Duration(1<<n)
-			fmt.Printf("⚠️[Error Exponential] (%v)\n", delay)
-			return delay
-
-		}
-		// if err.Error() == entity.ERRUnusual.Error() {
-		// 	fmt.Printf("⚠️ [Error unusual case, retry immeadiatly ")
-		// 	return 100 * time.Millisecond
-
-		// } else if err.Error() == entity.ERRBusy1.Error() {
-		// 	fmt.Printf("⚠️[Error Aware Delay]Database Lock Detected: Long Delay (%v)\n", 3*time.Second)
-		// 	return 3 * time.Second
-
-		// } else if err.Error() == entity.ERRBusy2.Error() {
-		// 	fmt.Printf("⚠️[Error Exponential]")
-		// 	baseDelay := 500 * time.Millisecond
-		// 	return baseDelay * time.Duration(1<<n)
-
-		// }
-
-		// สำหรับ Error ชนิดอื่นๆ ที่ไม่ได้ระบุ ให้ใช้ Fixed Delay (เช่น 1 วินาที)
-		return 1 * time.Second
-	})
-
-	delayType := retry.DelayType(delayTypeFunc)
-
-	retrier := retry.New(retryAttempt, onretry, delayType, retryIf)
+	retrier := retry.New(
+		delay,
+		maxJitter,
+		maxDelay,
+		retryAttempt,
+		attemptsForError,
+		delayType,
+		retryIf,
+		onretry,
+	)
 
 	return &GoRetry{
 		Retrier: retrier,
 	}
+}
+
+func delayFunction(n uint, err error, config retry.DelayContext) time.Duration {
+
+	switch err.Error() {
+	case entity.ERRUnusual.Error():
+		fmt.Printf("⚠️ [Error unusual case, retry immeadiatly\n")
+		return 100 * time.Millisecond
+
+	case entity.ERRBusy1.Error():
+		fmt.Printf("⚠️[Error Aware Delay]Database Lock Detected: Long Delay (%v)\n", 3*time.Second)
+		return 3 * time.Second
+
+	case entity.ERRBusy2.Error():
+		baseDelay := (500 * time.Millisecond)
+		delay := baseDelay * time.Duration(1<<n)
+		fmt.Printf("⚠️[Error Exponential] (%v)\n", delay)
+		return delay
+
+	}
+	return 1 * time.Second
 }
